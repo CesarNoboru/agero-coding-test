3a44f8172b75109348e3a97fa1f54da1ed86cd5c
Cesar Noboru <cesar.lira.3.glo@globalhitss.com.br>
2024-01-18 19:39:14 -0300

    adding log_level env variable logic

--- python/json_logger.py
+++ python/json_logger.py
@@ -1,5 +1,6 @@
 import logging
 from pythonjsonlogger import jsonlogger
+import os
 
 handler = logging.StreamHandler()
 format_str = '%(asctime)%(name)%(levelname)%(message)%(lineno)%(funcName)'
@@ -7,5 +8,9 @@ formatter = jsonlogger.JsonFormatter(format_str)
 handler.setFormatter(formatter)
 logger = logging.getLogger('AgeroExercise')
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+if "log_level" in os.environ:
+    log_level = getattr(logging, os.environ["log_level"].upper())
+    logger.setLevel(log_level)
+else:
+    logger.setLevel(logging.DEBUG)
 logger.propagate = False
