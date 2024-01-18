823117dc960dc12ca53f21346e6d5fde15402c37
Cesar Noboru <cesar.lira.3.glo@globalhitss.com.br>
2024-01-18 18:40:26 -0300

    getting column name from the DF itself

--- python/process_csv.py
+++ python/process_csv.py
@@ -20,7 +20,9 @@ class CSVProcess:
             raise Exception(f"Error getting threshold from ENV variables: {e}")
         
         try:
-            result = dataframe[dataframe['random_nums'] > threshold]
+            column_name = dataframe.columns[0]
+            logger.debug(f"Column name: {column_name}")
+            result = dataframe[dataframe[column_name] > threshold]
             logger.debug(result.to_string(index=False))
         except Exception as e:
             logger.error(f"Error filtering CSV with threshold: {e}")
