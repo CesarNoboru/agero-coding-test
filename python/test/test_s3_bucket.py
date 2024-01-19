e9a2082f9c82c0755dbc0bedee59fcebdf2e1c50
Cesar Noboru <cesar.lira.3.glo@globalhitss.com.br>
2024-01-19 14:17:58 -0300

    adding regex match to ensure the error + more tests

--- python/test/test_s3_bucket.py
+++ python/test/test_s3_bucket.py
@@ -35,7 +35,7 @@ def s3_test(s3_client, bucket_name):
     s3_client.create_bucket(Bucket=bucket_name)
     yield
 
-@pytest.mark.order(2)
+@pytest.mark.order(3)
 def test_send(s3_client, s3_test):
     test_data = StringIO(values.VALID_CSV)
     df = pd.read_csv(test_data, engine="python", encoding="utf-8")
@@ -43,16 +43,7 @@ def test_send(s3_client, s3_test):
     os.environ["destination_bucket"] = "my-test-bucket"
     assert my_client.send("test-file", df) == True
 
-@pytest.mark.order(1)
-def test_send_error(s3_client, s3_test):
-    test_data = StringIO(values.VALID_CSV)
-    df = pd.read_csv(test_data, engine="python", encoding="utf-8")
-    my_client = S3Client()
-    with pytest.raises(Exception):
-        my_client.send("test-file", df)
-        os.environ["destination_bucket"] = "wrong-bucket"
-
-@pytest.mark.order(3)
+@pytest.mark.order(4)
 def test_get(s3_client, s3_test):
     with NamedTemporaryFile(delete=True, suffix=".csv") as tmp:
         with open(tmp.name, "w", encoding="UTF-8") as f:
@@ -64,14 +55,31 @@ def test_get(s3_client, s3_test):
     csv = s3.get("my-test-bucket", tmp.name)
     assert csv.read().decode('utf-8') == values.VALID_CSV
 
-@pytest.mark.order(4)
+@pytest.mark.order(1)
+def test_send_no_bucket_env(s3_client, s3_test):
+    test_data = StringIO(values.VALID_CSV)
+    df = pd.read_csv(test_data, engine="python", encoding="utf-8")
+    my_client = S3Client()
+    with pytest.raises(Exception, match=r"Error getting destination bucket from ENV variables:.*"):
+        my_client.send("test-file", df)
+
+@pytest.mark.order(2)
+def test_send_no_bucket(s3_client, s3_test):
+    test_data = StringIO(values.VALID_CSV)
+    df = pd.read_csv(test_data, engine="python", encoding="utf-8")
+    my_client = S3Client()
+    os.environ["destination_bucket"] = "no-bucket"
+    with pytest.raises(Exception, match=r"Error sending Object to DESTINATION S3:.*"):
+        my_client.send("test-file", df)
+
+@pytest.mark.order(6)
 def test_get_error_bucket(s3_client, s3_test):
     s3 = S3Client()
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match=r"Error getting Object from SOURCE S3:.*"):
         s3.get("no-bucket", "whatever")
 
 @pytest.mark.order(5)
 def test_get_error_key(s3_client, s3_test):
     s3 = S3Client()
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match=r"Error getting Object from SOURCE S3:.*"):
         s3.get("my-test-bucket", "whatever")
