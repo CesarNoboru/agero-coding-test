b8a78e7120f0b1c95d01ad492dfaee5fd01c82fb
Cesar Noboru <cesar.lira.3.glo@globalhitss.com.br>
2024-01-19 13:57:07 -0300

    adding custom field with amazon request id

--- python/json_logger.py
+++ python/json_logger.py
@@ -1,10 +1,26 @@
 import logging
 from pythonjsonlogger import jsonlogger
 import os
+from datetime import datetime
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+        if "request_id" not in os.environ:
+            os.environ["request_id"] = ""
+        log_record["request_id"] = os.environ["request_id"]
+
 
 handler = logging.StreamHandler()
-format_str = '%(asctime)%(name)%(levelname)%(message)%(lineno)%(funcName)'
-formatter = jsonlogger.JsonFormatter(format_str)
+format_str = '%(timestamp)%(name)%(level)%(message)%(funcName)%(request_id)'
+formatter = CustomJsonFormatter(format_str)
 handler.setFormatter(formatter)
 logger = logging.getLogger('AgeroExercise')
 logger.addHandler(handler)
